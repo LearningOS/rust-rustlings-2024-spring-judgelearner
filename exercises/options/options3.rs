9340654eb7ee6b32870ba2af3109141b81a215d2
judgelearner <675727394@qq.com>
2024-04-24 22:36:34 +0800

    list

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -3,7 +3,7 @@
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 struct Point {
     x: i32,
@@ -11,11 +11,11 @@ struct Point {
 }
 
 fn main() {
-    let y: Option<Point> = Some(Point { x: 100, y: 200 });
+    let ref y: Option<Point> = Some(Point { x: 100, y: 200 });
 
     match y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
-    y; // Fix without deleting this line.
+     y; // Fix without deleting this line.
 }
